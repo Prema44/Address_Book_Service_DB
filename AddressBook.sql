3bff14b9d1ebc748181dce7b6e288ae7612b5f6b
Sakshat Bagadi <bagadisakshat@gmail.com>
2020-10-30 20:33:15 +0530

    Added: Query to create AddressBook table with Required Fields

--- AddressBook.sql
+++ AddressBook.sql
@@ -2,3 +2,19 @@
 CREATE DATABASE AddressBookService;
 SHOW DATABASES;
 USE addressbookservice;
+
+#UC2
+ CREATE TABLE AddressBook
+    -> (
+    -> firstName varchar(100) NOT NULL,
+    -> lastName varchar(100) NOT NULL,
+    -> address varchar(250) NOT NULL,
+    -> city varchar(50) NOT NULL,
+    -> state varchar(50) NOT NULL,
+    -> zip numeric(6),
+    -> phone numeric(10) NOT NULL,
+    -> email varchar(100) NOT NULL,
+    -> primary key (firstName, lastName)
+    -> );
+
+ DESCRIBE Addressbook;
